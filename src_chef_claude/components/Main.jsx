19e8c01fcd39232761f9abaa5f8817888bd1d3dc
annadovzhenko <adovzhenko@vmware.com>
2025-07-29 21:42:47 +0200

    added scroll to generated recipe with useRef and useEffect hooks

--- src_chef_claude/components/Main.jsx
+++ src_chef_claude/components/Main.jsx
@@ -4,12 +4,21 @@ import ClaudeRecipe from "./ClaudeRecipe"
 import { getRecipeFromMistral } from "../ai"
 
 export default function Main() {
-    const [ingredients, setIngredients] = React.useState([]);
-    const [recipe, setRecipe] = React.useState("");
+    const [ingredients, setIngredients] = React.useState(
+        ["chicken", "all the main spices", "corn", "heavy cream", "pasta"]
+    )
+    const [recipe, setRecipe] = React.useState("")
+    const recipeSection = React.useRef(null)
+
+    React.useEffect(() => {
+        if (recipe !== "" && recipeSection.current !== null) {
+            recipeSection.current.scrollIntoView({behavior: "smooth"})
+        }
+    }, [recipe])
 
     async function getRecipe() {
         const recipeMarkdown = await getRecipeFromMistral(ingredients)
-        setRecipe(recipeMarkdown);
+        setRecipe(recipeMarkdown)
     }
 
     function addIngredient(formData) {
@@ -31,6 +40,7 @@ export default function Main() {
 
             {ingredients.length > 0 &&
                 <IngredientsList
+                    ref={recipeSection}
                     ingredients={ingredients}
                     getRecipe={getRecipe}
                 />
